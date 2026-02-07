13fcc2ec929f34afbe27fb7173ed23524a70225d
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2026-02-07 18:58:40 +0700

    feat: страница для несуществующих путей

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,7 +1,14 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  NavLink,
+} from "react-router-dom";
 import { MainLayout } from "@/widgets/mainLayout";
 import { NotesList } from "@/pages/notesList";
 import { routes } from "@/shared/router/routes";
+import { Result } from "antd";
 
 export const AppRouter = () => {
   return (
@@ -12,6 +19,22 @@ export const AppRouter = () => {
         </Route>
 
         <Route path="/" element={<Navigate to={routes.noteList.path} replace />} />
+
+        <Route
+          path={routes.allNotExistingRoutes.path}
+          element={<Navigate to={routes.notFound.path} replace />}
+        />
+        <Route
+          path={routes.notFound.path}
+          element={
+            <Result
+              extra={<NavLink to={routes.noteList.path}>К списку заметок</NavLink>}
+              status={404}
+              title="404"
+              subTitle="Такой страницы не существует"
+            />
+          }
+        />
       </Routes>
     </Router>
   );
